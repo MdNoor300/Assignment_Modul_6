c26ebd82b66eb773341dabb03004164836e1b56f
MdNoor300 <sehzad.noor98@gmail.com>
2023-11-06 01:22:58 +0600

    Add Query

---
+++ Customers_Info.sql
@@ -0,0 +1,38 @@
+--Task1:
+SELECT c.customer_id, c.name, c.email, c.location, COUNT(o.order_id) AS total_orders
+FROM Customers c
+LEFT JOIN Orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.name, c.email, c.location
+ORDER BY total_orders DESC;
+
+
+--Task2:
+SELECT o.order_id, p.name AS product_name, oi.quantity, (oi.quantity * oi.unit_price) AS total_amount
+FROM Order_Items oi
+JOIN Orders o ON oi.order_id = o.order_id
+JOIN Products p ON oi.product_id = p.product_id
+ORDER BY o.order_id ASC;
+
+
+
+--Task3:
+SELECT c.name AS category_name, SUM(oi.quantity * oi.unit_price) AS total_revenue
+FROM Order_Items oi
+JOIN Products p ON oi.product_id = p.product_id
+JOIN Categories c ON category_id = c.category_id
+GROUP BY c.name
+ORDER BY total_revenue DESC;
+
+
+
+
+--Task4:
+SELECT c.name AS customer_name, SUM(oi.quantity * oi.unit_price) AS total_purchase_amount
+FROM Customers c
+LEFT JOIN Orders o ON c.customer_id = o.customer_id
+LEFT JOIN Order_Items oi ON o.order_id = oi.order_id
+GROUP BY c.name
+ORDER BY total_purchase_amount DESC
+LIMIT 5;
+
+
