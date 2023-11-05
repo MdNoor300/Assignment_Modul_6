6c3089dae89cd8a1f76ee1f98605f22cd5bb84a1
MdNoor300 <sehzad.noor98@gmail.com>
2023-11-06 01:25:35 +0600

    Add Query

--- Customers_Info.sql
+++ Customers_Info.sql
@@ -19,7 +19,7 @@ ORDER BY o.order_id ASC;
 SELECT c.name AS category_name, SUM(oi.quantity * oi.unit_price) AS total_revenue
 FROM Order_Items oi
 JOIN Products p ON oi.product_id = p.product_id
-JOIN Categories c ON category_id = c.category_id
+JOIN Categories c ON c.category_id = c.category_id
 GROUP BY c.name
 ORDER BY total_revenue DESC;
 
